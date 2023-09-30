5f0a7f411f04b16e9d3a442713bd5b7e69bc28b5
FelipeAriki <felipeariki3@gmail.com>
2023-09-30 17:47:03 -0300

    feat: Exclusao e Edição dos clientes

--- protegido/scripts/script.js
+++ protegido/scripts/script.js
@@ -1,10 +1,10 @@
 const form = document.getElementById('formCliente');
-window.onload = ()=>{
+window.onload = () => {
     obterClientes();
 }
 //Evento nos botões
-document.querySelector("#btnCadastrar").addEventListener("click", ()=>{    
-    if(form.checkValidity()){
+document.querySelector("#btnCadastrar").addEventListener("click", () => {
+    if (form.checkValidity()) {
         const cliente = obterClienteFormulario();
         cadastrarCliente(cliente);
         prepararTela();
@@ -13,13 +13,18 @@ document.querySelector("#btnCadastrar").addEventListener("click", ()=>{
         form.classList.add('was-validated');
 });
 
-document.querySelector("#btnVoltar").addEventListener("click", ()=>{
+document.querySelector("#btnVoltar").addEventListener("click", () => {
     history.back();
 });
-
+document.querySelector("#btnExcluir").addEventListener("click", ()=>{
+    removerCliente();
+});
+document.querySelector("#btnEditar").addEventListener("click", ()=>{
+    editarCliente();
+});
 
 //Funções
-function limparFormulario(){
+function limparFormulario() {
     document.querySelector("#cpf").value = '';
     document.querySelector("#nome").value = '';
     document.querySelector("#sobrenome").value = '';
@@ -31,7 +36,7 @@ function limparFormulario(){
     document.querySelector("#email").value = '';
     form.classList.remove('was-validated');
 }
-function obterClienteFormulario(){
+function obterClienteFormulario() {
     let cpf = document.querySelector("#cpf").value;
     let nome = document.querySelector("#nome").value;
     let sobrenome = document.querySelector("#sobrenome").value;
@@ -42,9 +47,8 @@ function obterClienteFormulario(){
     let telefone = document.querySelector("#telefone").value;
     let email = document.querySelector("#email").value;
 
-    if(cpf && nome && sobrenome && endereco && bairro && cidade && uf && telefone && email)
-    {
-        return{
+    if (cpf && nome && sobrenome && endereco && bairro && cidade && uf && telefone && email) {
+        return {
             cpf: cpf,
             nome: nome,
             sobrenome: sobrenome,
@@ -59,67 +63,66 @@ function obterClienteFormulario(){
     else
         return undefined;
 }
-function obterClientes(){
-    fetch('https://129.146.68.51/aluno25-ppiadsead/clientes', {method: 'GET'})
-    .then((res)=>{
-        if(res.status === 200){
-            return res.json();
-        }
-        else{
-            return [];
-        }
-    })
-    .then((listaClientes)=>{
-        mostrarClientes(listaClientes);
-    })
-    .catch((error)=>{
-        mostrarMensagem('Não foi possível obter os clientes. Erro: ' + error.message, 'danger');
-    });
-}
-function cadastrarCliente(cliente){
-    fetch('https://129.146.68.51/aluno25-ppiadsead/clientes',
-    {
-        method: 'POST',
-        headers:{
-            'Content-Type':'application/json'
-        },
-        body: JSON.stringify(cliente)
-    })
-    .then((res)=>{
-        if(res.status === 200)
-            return res.json();
-        else{
-            return{
-                status:false,
-                mensagem:"Oops, Não foi possível realizar o cadastro do cliente!"
+function obterClientes() {
+    fetch('https://129.146.68.51/aluno25-ppiadsead/clientes', { method: 'GET' })
+        .then((res) => {
+            if (res.status === 200) {
+                return res.json();
             }
-        }
-    })
-    .then((respostaBackend)=>{
-        let flag = '';
-        if(respostaBackend.status){
-            flag = 'success';
-            obterClientes();
-        }            
-        else
-            flag = 'danger';
-
-        mostrarMensagem(respostaBackend.mensagem, flag);
-    })
-    .catch((error)=>{
-        mostrarMensagem('Não foi possível realizar o cadastro do cliente. Erro: ' + error.message, 'danger');
-    });
-
+            else {
+                return [];
+            }
+        })
+        .then((listaClientes) => {
+            mostrarClientes(listaClientes);
+        })
+        .catch((error) => {
+            mostrarMensagem('Não foi possível obter os clientes. Erro: ' + error.message, 'danger');
+        });
 }
-function mostrarClientes(listaClientes){
+function cadastrarCliente(cliente) {
+    fetch('https://129.146.68.51/aluno25-ppiadsead/clientes',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(cliente)
+        })
+        .then((res) => {
+            if (res.status === 200)
+                return res.json();
+            else {
+                return {
+                    status: false,
+                    mensagem: "Oops, Não foi possível realizar o cadastro do cliente!"
+                }
+            }
+        })
+        .then((respostaBackend) => {
+            let flag = '';
+            if (respostaBackend.status) {
+                flag = 'success';
+                obterClientes();
+            }
+            else
+                flag = 'danger';
+
+            mostrarMensagem(respostaBackend.mensagem, flag);
+        })
+        .catch((error) => {
+            mostrarMensagem('Não foi possível realizar o cadastro do cliente. Erro: ' + error.message, 'danger');
+        });
+}
+function mostrarClientes(listaClientes) {
     let elementoDivTabela = document.querySelector("#espacoTabela");
-    if(listaClientes.length > 0){
+    if (listaClientes.length > 0) {
         //Limpar o elemento divTabela
         elementoDivTabela.innerHTML = '';
-        let tabela          = document.createElement('table');
+        let tabela = document.createElement('table');
         tabela.className = 'table table-striped table-hover';
         let cabecalhoTabela = document.createElement('thead');
-        let corpoTabela     = document.createElement('tbody')
+        let corpoTabela = document.createElement('tbody')
 
         cabecalhoTabela.innerHTML = `<tr>
                                         <th>CPF</th>
@@ -135,7 +138,7 @@ function mostrarClientes(listaClientes){
                                     </tr>`;
         tabela.appendChild(cabecalhoTabela);
 
-        for(const cliente of listaClientes){
+        for (const cliente of listaClientes) {
             let linhaTabela = document.createElement('tr');
             linhaTabela.innerHTML = `<td>${cliente.cpf}</td>
                                     <td>${cliente.nome}</td>
@@ -153,19 +156,19 @@ function mostrarClientes(listaClientes){
         tabela.appendChild(corpoTabela);
         elementoDivTabela.appendChild(tabela);
     }
-    else{
+    else {
         elementoDivTabela.innerHTML = `<div class="alert alert-warning" role="alert">Nenhum Cliente Cadastrado!</div>`;
     }
 }
 
-function mostrarMensagem(mensagem, tipo){
+function mostrarMensagem(mensagem, tipo) {
     let divMensagem = document.querySelector("#mensagem");
     divMensagem.innerHTML = `<div class="alert alert-${tipo}" role="alert">${mensagem}</div>`;
     setTimeout(() => {
         divMensagem.innerHTML = '';
     }, 5000);
 }
-function prepararTela(cpf="", nome="", sobrenome="", endereco="", bairro="", cidade="", uf="", telefone="", email="", acao=""){
+function prepararTela(cpf = "", nome = "", sobrenome = "", endereco = "", bairro = "", cidade = "", uf = "", telefone = "", email = "", acao = "") {
     let btnCadastrar = document.querySelector("#btnCadastrar");
     let btnEditar = document.querySelector("#btnEditar");
     let btnExcluir = document.querySelector("#btnExcluir");
@@ -180,21 +183,106 @@ function prepararTela(cpf="", nome="", sobrenome="", endereco="", bairro="", cid
     document.querySelector("#telefone").value = telefone;
     document.querySelector("#email").value = email;
 
-    if(acao === "excluir"){
+    if (acao === "excluir") {
+        document.querySelector("#cpf").disabled = true;
         btnCadastrar.disabled = true;
         btnEditar.disabled = true;
         btnExcluir.disabled = false;
     }
-    else if(acao === "atualizar")
-    {
+    else if (acao === "atualizar") {
+        document.querySelector("#cpf").disabled = true;
         btnCadastrar.disabled = true;
         btnEditar.disabled = false;
         btnExcluir.disabled = true;
     }
-    else{//cadastrar
+    else {//cadastrar
+        document.querySelector("#cpf").disabled = false;
         btnCadastrar.disabled = false;
         btnEditar.disabled = true;
         btnExcluir.disabled = true;
         limparFormulario();
+    }
+}
+function removerCliente() {
+    if (confirm("Confirma a exclusão do cliente selecionado?")) {
+        fetch("https://129.146.68.51/aluno25-ppiadsead/clientes", {
+            method: 'DELETE',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                cpf: document.querySelector("#cpf").value
+            })
+        })
+            .then((res) => {
+                if (res.status === 200)
+                    return res.json();
+                else {
+                    return {
+                        status: false,
+                        mensagem: "Oops, Não foi possível realizar a exclusão do cliente!"
+                    }
+                }
+            })
+            .then((respostaBackend) => {
+                let flag = '';
+                if (respostaBackend.status) {
+                    flag = 'success';
+                    prepararTela();
+                    obterClientes();
+                }
+                else
+                    flag = 'danger';
+
+                mostrarMensagem(respostaBackend.mensagem, flag);
+            })
+            .catch((error) => {
+                mostrarMensagem('Não foi possível realizar a exclusão do cliente. Erro: ' + error.message, 'danger');
+            });
+    } else {
+        prepararTela();
+    }
+}
+function editarCliente() {
+    if (confirm("Confirma a edição do cliente?")) {
+        const cliente = obterClienteFormulario();
+        if(cliente){
+            fetch('https://129.146.68.51/aluno25-ppiadsead/clientes',
+            {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(cliente)
+            })
+            .then((res) => {
+                if (res.status === 200)
+                    return res.json();
+                else {
+                    return {
+                        status: false,
+                        mensagem: "Oops, Não foi possível realizar a edição do cliente!"
+                    }
+                }
+            })
+            .then((respostaBackend) => {
+                let flag = '';
+                if (respostaBackend.status) {
+                    flag = 'success';
+                    obterClientes();
+                }
+                else
+                    flag = 'danger';
+
+                mostrarMensagem(respostaBackend.mensagem, flag);
+            })
+            .catch((error) => {
+                mostrarMensagem('Não foi possível realizar a edição do cliente. Erro: ' + error.message, 'danger');
+            });
+        }
+        else{
+            mostrarMensagem('Não foi possível realizar a edição do cliente, favor informar seus dados corretamente', 'danger')
+        }
+    }
+    else{
+        prepararTela();
     }
 }
